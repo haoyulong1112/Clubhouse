8bc819e07ac185de8b9a38b66e3b361a5dfad47c
hyl <10321970@qq.com>
2021-03-25 22:23:19 +0800

    update src/pages/master/masterset/masterset.vue

---
+++ src/api/admaster/master.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+const requestDomain = envConfig.requestDomain
+
+// 添加管理员
+export const addAdmin = data => {
+    return request({
+        url: `${requestDomain}/club-room/admin/addAdmin`,
+        data: data
+    })
+}
+// 获取管理员列表  
+export const adminList = data => {
+    return request({
+        url: `${requestDomain}/club-room/admin/adminList`,
+        data: data
+    })
+}
+// 更新管理员
+export const updateAdmin = data => {
+    return request({
+        url: `${requestDomain}/club-room/admin/updateAdmin`,
+        data: data
+    })
+}
+
+// 删除管理员
+export const delAdmin = data => {
+    return request({
+        url: `${requestDomain}/club-room/admin/delAdmin`,
+        data: data
+    })
+}
--- src/pages/master/masterset/masterset.vue
+++ src/pages/master/masterset/masterset.vue
@@ -7,13 +7,13 @@
                         <div class="master">
                             <el-form :model="masterForm" :rules="rules1" ref="masterForm" label-width="100px" class="master-ruleForm">
                                 <el-form-item label="添加管理员" class="master-formitem" prop="name">
-                                    <el-input class="master-input" placeholder="输入姓名" type="text" v-model="masterForm.name" autocomplete="off"></el-input>
+                                    <el-input class="master-input" placeholder="输入姓名" type="text" autocomplete="off" v-model.trim="masterForm.name"></el-input>
                                 </el-form-item>
                                 <el-form-item class="master-formitem" prop="phone">
-                                    <el-input class="master-input" placeholder="输入手机号" type="text" v-model="masterForm.phone" autocomplete="off"></el-input>
+                                    <el-input class="master-input" placeholder="输入手机号" type="text" autocomplete="off" v-model.trim="masterForm.phone"></el-input>
                                 </el-form-item>
                                 <el-form-item class="master-formitem" prop="password">
-                                    <el-input class="master-input" placeholder="输入密码" type="text" v-model="masterForm.password" autocomplete="off"></el-input>
+                                    <el-input class="master-input" placeholder="输入密码" type="text" autocomplete="off" v-model.trim="masterForm.password"></el-input>
                                 </el-form-item>
                                 <el-form-item class="master-btnbox">
                                     <el-button class="master-btn" @click="submitForm('masterForm')">添加</el-button>
@@ -22,32 +22,11 @@
                         </div>
                     </div>
                 </el-col>
-                <!-- <el-col :span="12">
-                    <div class="grid-content bg-purple">
-                        <div class="clubmaster">
-                            <el-form :model="clubForm" :rules="rules2" ref="clubForm" label-width="100px" class="master-ruleForm">
-                                <el-form-item label="俱乐部管理员" class="master-formitem" prop="name">
-                                    <el-input class="master-input" placeholder="输入姓名" type="text" v-model="clubForm.name" autocomplete="off"></el-input>
-                                </el-form-item>
-                                <el-form-item class="master-formitem" prop="phone">
-                                    <el-input class="master-input" placeholder="输入手机号" type="text" v-model="clubForm.phone" autocomplete="off"></el-input>
-                                </el-form-item>
-                                <el-form-item class="master-formitem" prop="password">
-                                    <el-input class="master-input" placeholder="输入密码" type="text" v-model="clubForm.password" autocomplete="off"></el-input>
-                                </el-form-item>
-                                <el-form-item class="master-btnbox">
-                                    <el-button class="master-btn" @click="submitForm('clubForm')">添加</el-button>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                    </div>
-                </el-col> -->
             </el-row>
         </div>
         <div class="list-box">
             <div class="tab">
                 <div class="tab-item active">管理员 <span>8人</span></div>
-                <!-- <div :class=" currentTab == 2 ? 'tab-item active': 'tab-item'" @click="changeTab(2)">俱乐部管理员 <span>22人</span></div> -->
             </div>
             <div class="tab-content">  
                 <div class="user-box">  
@@ -58,57 +37,28 @@
                         <el-table-column prop="id" label="ID" width="60"></el-table-column>
                         <el-table-column label="昵称" width="220">
                             <template slot-scope="scope">
-                                <input class="table-input" type="text" name="nikeame" v-model="scope.row.name">
+                                <input class="table-input" type="text" name="nikeame" v-model.trim="scope.row.name">
                             </template>
                         </el-table-column>
                         <el-table-column prop="phone" label="手机号" width="220">
                             <template slot-scope="scope">
-                                <input class="table-input" type="text" name="phone" v-model="scope.row.phone">
+                                <input class="table-input" type="text" name="phone" v-model.trim="scope.row.phone">
                             </template>
                         </el-table-column>
                         <el-table-column prop="password" label="密码" width="220">
                             <template slot-scope="scope">
-                                <input class="table-input" type="text" name="password" v-model="scope.row.password">
+                                <input class="table-input" type="text" name="password" v-model.trim="scope.row.password">
                             </template>
                         </el-table-column>
                         <el-table-column label="操作">
-                            <template>
+                            <template slot-scope="scope">
                                 <div class="opation">
-                                    <span>保存</span>/
-                                    <span>删除</span>
+                                    <span @click="update(scope.row)">保存</span>/
+                                    <span @click="deleted(scope.row)">删除</span>
                                 </div>
                             </template>
                         </el-table-column>
                     </el-table>
-                    <!-- <el-table v-if="currentTab == 2 ? true : false" class="list-table"
-                        :data="tableData2"
-                        highlight-current-row
-                        style="width: 100%">
-                        <el-table-column prop="id" label="ID" width="60"></el-table-column>
-                        <el-table-column prop="name" label="昵称">
-                            <template slot-scope="scope" width="220">
-                                <input class="table-input" type="text" name="nikeame" v-model="scope.row.name">
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="phone" label="手机号" width="220">
-                            <template slot-scope="scope">
-                                <input class="table-input" type="text" name="phone" v-model="scope.row.phone">
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="password" label="密码" width="220">
-                            <template slot-scope="scope">
-                                <input class="table-input" type="text" name="password" v-model="scope.row.password">
-                            </template>
-                        </el-table-column>
-                        <el-table-column label="操作">
-                            <template>
-                                <div class="opation">
-                                    <span>保存</span>/
-                                    <span>删除</span>
-                                </div>
-                            </template>
-                        </el-table-column>
-                    </el-table> -->
                 </div>
             </div>
             <div class="block">
@@ -116,7 +66,7 @@
                 :background="true"
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
-                :current-page="currentPage"
+                :current-page="pageNo"
                 :page-sizes="[10, 20, 30]"
                 :page-size="pageSize"
                 :pager-count="9"
@@ -129,6 +79,7 @@
 </template>
 
 <script>
+import { addAdmin ,adminList,updateAdmin, delAdmin} from '@/api/admaster/master.js'
   export default{
     name: 'masterset',
     data() {
@@ -154,86 +105,28 @@
                     { required: true, message: '请填写密码', trigger: 'blur' }
                 ]
             },
-            rules2: {
-                name: [
-                    { required: true, message: '请填写姓名', trigger: 'blur' }
-                ],
-                phone: [
-                    { required: true, message: '请填写手机号', trigger: 'blur' }
-                ],
-                newPapasswordssword: [
-                    { required: true, message: '请填写密码', trigger: 'blur' }
-                ]
-            },
+            // rules2: {
+            //     name: [
+            //         { required: true, message: '请填写姓名', trigger: 'blur' }
+            //     ],
+            //     phone: [
+            //         { required: true, message: '请填写手机号', trigger: 'blur' }
+            //     ],
+            //     newPapasswordssword: [
+            //         { required: true, message: '请填写密码', trigger: 'blur' }
+            //     ]
+            // },
             total: 100,
-            currentPage: 1,
+            pageNo: 1,
             pageSize: 10,
             currentTab: 1,
-            tableData1: [
-                {
-                    id: '001',
-                    name: '张云',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '002',
-                    name: '登记卡',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '003',
-                    name: '李丹妮',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '004',
-                    name: '都解决',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '005',
-                    name: '张云',
-                    phone: '17839392020',
-                    password: '2222234',
-                }
-            ],
+            tableData1: [],
             tableData2: [
-                {
-                    id: '001',
-                    name: '非凡哥',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '002',
-                    name: '滚滚滚',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '003',
-                    name: '呵呵呵呵',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '004',
-                    name: '电风扇',
-                    phone: '17839392020',
-                    password: '2222234',
-                },
-                {
-                    id: '001',
-                    name: '张云',
-                    phone: '17839392020',
-                    password: '2222234',
-                }
             ]
         }
+    },
+    created() {
+        this.getAdmasterList();
     },
     methods: {
             handleSizeChange(val){
@@ -244,6 +137,86 @@
             },
             changeTab(index){
                 this.currentTab = index;
+            },
+            // 添加管理员
+            submitForm(str){
+                this.$refs[str].validate((flag) => {
+                    if(flag){
+                        let data = {
+                            name: this.masterForm.name,
+                            password: this.masterForm.password,
+                            phone: this.masterForm.phone,
+                        }
+                        console.log(data);
+                        addAdmin(data).then(res => {
+                            if(res.code == 200){
+                                this.$message({
+                                    message: '添加成功',
+                                    type: 'success'
+                                });
+                                this.getAdmasterList();
+                            }
+                        }).catch(err => {
+                            this.$message.error(err.msg);
+                        })
+                    }
+                })
+            },
+            // 获取管理员列表
+            getAdmasterList(){
+                let data = {
+                    pageNo: this.pageNo,
+                    pageSize: this.pageSize
+                }
+                adminList(data).then(res => {
+                    if(res.code == 200){
+                        this.tableData1 = res.data;
+                    }
+                })
+            },
+            // 更新管理员
+            update(row){
+                console.log(row);
+                let data = {
+                    id: row.id,
+                    name: row.name,
+                    password: row.password,
+                    phone: row.phone,
+                }
+                if(!data.id){
+                    this.$message.error('缺少参数');
+                }
+                console.log(data);
+                updateAdmin(data).then(res => {
+                    if(res.code == 200){
+                        this.$message({
+                            message: '更新成功',
+                            type: 'success'
+                        });
+                    }
+                }).catch(err => {
+                    this.$message.error(err.msg);
+                })
+            },
+            deleted(row){
+                let data = {
+                    id: row.id,
+                }
+                if(!data.id){
+                    this.$message.error('缺少参数');
+                }
+                console.log(data);
+                delAdmin(data).then(res => {
+                    if(res.code == 200){
+                        this.$message({
+                            message: '删除成功',
+                            type: 'success'
+                        });
+                        this.getAdmasterList();
+                    }
+                }).catch(err => {
+                    this.$message.error(err.msg);
+                })
             }
     },
   }
@@ -252,11 +225,6 @@
     .container
         width 100%;
         background-color: #fff;
-        // overflow hidden;
-        // height 100%;
-        // padding-bottom: 30px
-        // margin-bottom 50px
-        // box-sizing border-box;
     .addmaster
         width 100%;
         padding-top 40px;
