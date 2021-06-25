6512d76d4a4d592205d8d0a1497726601a788f30
hyl <10321970@qq.com>
2021-06-25 17:41:23 +0800

    update favicon.ico

--- static/config/index.js
+++ static/config/index.js
@@ -1,67 +1,33 @@
 (function (window) {
     // 区分环境的配置 252 demo prod
-    var testConfig = {
-        // 请求域名头 pet-pai
-        // requestDomain: 'http://192.168.1.151:8200',
-        // 跨域
-        requestDomain: '/test',
-    }
     var demoConfig = {
         // 请求域名头 pet-pai
         requestDomain: 'http://8.141.49.230:8200',
         // 跨域
         // requestDomain: '/demo',
     }
-
     var prodConfig = {
         // 请求域名头 pet-pai
-        // requestDomain: 'https://api.ichongwujia.com',
-        // 跨域
-        requestDomain: '/test',
+        requestDomain: 'http://ch.huitingdata.com',
+        // 分享appid
+        // shareAppid: '',
+        // 分享域名头
+        shareDomain: 'http://www.huitingdata.com',
+        // 管理域名头
+        manageDomain: ''
     }
+
     window.SITE_CONFIG = {};
     
     // 固定的配置
     var config = {
         // rem布局页面最大宽度
         pageWidth: 1920,
-        // // 头像
-        // userAvatar: 'http://dev-pet-avatar.oss-cn-beijing.aliyuncs.com/html5/15655992129449057657.png',
-        // // 分享图片
-        // shareImg: 'http://dev-pet-avatar.oss-cn-beijing.aliyuncs.com/html5/15690470902670425207.png',
-        // // favicon
-        // favicon: 'https://dev-pet-avatar.oss-cn-beijing.aliyuncs.com/html5/15690470902670425207.png',
-        // // 跳转应用宝下载APP
-        // downloadApp: function () { window.location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.haotang.pet&g_f=991653' },
-        // // 下载组件logo
-        // downloadLogo: 'http://dev-pet-avatar.oss-cn-beijing.aliyuncs.com/html5/15655992124122270752.png',
-        // // 下载组件title
-        // downloadTitle: '宠物家',
-        // // 下载组件description
-        // downloadDescription: '专注您的宠物生活',
-        // // 验证登录手机号正则
-        // cellPhoneReg: /^1[0-9]{10}$/,
-        // // 客服电话
-        // kfnum: '400-030-0011',
-        // kfnumber: '4000300011',
-        // // 服务端返回的图片地址兼容性处理
-        // getFullImgUrl: function (string) {
-        //     if (string && typeof string === 'string') {
-        //         return /^http/.test(string) ? string : envConfig.requestDomain + string
-        //     }
-        // },
-        // // 宠物头像图片地址兼容性处理
-        // getFullPetAvatar: function (string) {
-        //     if (string && typeof string === 'string') {
-        //         return /^http/.test(string) ? string : 'http://img.cwjia.cn/static/pavatar/' + string
-        //     }
-        // }
     }
 
     // 通过hostname区分环境  http://localhost:8080/
-    var testDomainArray = ['localhost']
     var demoDomainArray = ['8.141.49.230']
-    var prodDomainArray = ['192.168.1.188']
+    var prodDomainArray = ['www.huitingdata.com']
 
     var hostname = window.location.hostname
 
@@ -76,14 +42,11 @@
 
     window.config = config
     window.envConfig = {}
-    if (isItemInStr(hostname, testDomainArray)) {
-        window.envConfig = testConfig
-    }
     if (isItemInStr(hostname, demoDomainArray)) {
         window.envConfig = demoConfig
     }
     if (isItemInStr(hostname, prodDomainArray)) {
         window.envConfig = prodConfig
     }
-    console.log(window.envConfig);
+    console.log(window.envConfig)
 }(window))
